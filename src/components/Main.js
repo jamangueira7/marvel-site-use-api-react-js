37ef76ed0fa313597d2f62d34c73a017c5e3e77a
João Mangueira <jamjs7@gmail.com>
2023-03-15 16:10:37 -0300

    build(Main): buscando os dados da Marvel API

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,23 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import { Card } from "./Card";
+import axios from "axios";
 
 export const Main = () => {
+    const md5hash = `c47295561127eec2e6efc378a64f0d4a`;
+    const publicKey = `87972729642c1e41dcba909747394e95`;
+    const baseURL = `http://gateway.marvel.com/v1/public/characters?ts=1&apikey=${publicKey}&hash=${md5hash}`;
+
+    const [url, setUrl] = useState(baseURL);
+    const [item, setItem] = useState();
+
+    useEffect(() => {
+        const fetch = async () => {
+            const res = await axios.get(url);
+            setItem(res.data.data.results)
+        }
+
+        fetch();
+    }, [url]);
     return (
         <>
             <header>
