04234ca5d74b04c564961833a9c69ef15878862c
João Mangueira <jamjs7@gmail.com>
2023-03-15 16:53:47 -0300

    build: add as rotas

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import { Routes, Route } from "react-router-dom";
 import { Main } from "./components/Main";
+import { Marvel } from "./components/Marvel";
 import "./components/style.css";
 
 function App() {
   return (
     <>
-      <Main />
+        <Routes>
+            <Route path="/" element={<Main />}/>
+            <Route path='/:id' element={<Marvel/>}/>
+        </Routes>
     </>
   );
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,12 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
-export const Card = ({item}) => {
+export const Card = ({ item }) => {
+    let navigate = useNavigate();
+
     return (
         <>
-            <div className="card">
+            <div className="card" onClick={() => navigate(`/${item.id}`)} >
                 <img src={`${item.thumbnail.path}.${item.thumbnail.extension}`} alt=""/>
                 <div className="title">
                     <h3>{item.name}</h3>
